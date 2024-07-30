5c55795e8b73757c82b9842793cf40cffd2ad010
Nikhil Gurjar <nikhilgurjar122000@gmail.com>
2024-07-30 11:23:36 +0530

    Backend: Flights status can be updated by only the authenticated users

--- backend/src/api/v1/routes/FlightRoutes.js
+++ backend/src/api/v1/routes/FlightRoutes.js
@@ -3,9 +3,12 @@ const router = require('express').Router();
 //------------------- Controllers specific stuffs -------------------X
 const {FlightControllers} = require('../controllers');
 
+//------------ Middlewares specific stuffs
+const isAuthenticated = require('../middlewares/isAuthenticated'); //check is authentication user
+
 
 //------------------ Start to create the routes related to the flights
-router.put('/updateStatus', FlightControllers.updateFlightStatus); //update flight status 
+router.put('/updateStatus',isAuthenticated, FlightControllers.updateFlightStatus); //update flight status 
 router.get('/get', FlightControllers.getAllFlights); //update flight status 
 
 
